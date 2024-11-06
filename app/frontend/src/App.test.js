67804b043e9d4765aac925ae18fee6704092444b
Abdul Rahman Tkxel <muhammad.rehman@tkxel.com>
2024-11-06 15:50:47 +0500

    frontend jest configurations

--- app/frontend/src/App.test.js
+++ app/frontend/src/App.test.js
@@ -3,6 +3,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import { act } from 'react';
 
+
 test('renders learn react link', () => {
   act(() => {
     render(<App />);
